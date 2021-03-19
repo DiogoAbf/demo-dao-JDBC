ef5798e41f08faaa23fdcfb2ca7c47a046dab4d2
Diogo Augusto <dabf.augusto@gmail.com>
2021-03-19 10:52:07 -0300

    Departament entity class

---
+++ src/application/Progam.java
@@ -0,0 +1,13 @@
+package application;
+import model.entities.*;
+
+public class Progam {
+
+	public static void main(String[] args) {
+		Departament  obj = new Departament();
+		
+		System.out.println(obj);
+
+	}
+
+}
